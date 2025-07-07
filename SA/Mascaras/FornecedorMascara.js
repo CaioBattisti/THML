4ef2262f93f1a582dc78e52731c8efed88e42a4d
CaioBattisti <cluizbattisti@gmail.com>
2025-07-07 20:08:56 -0300

    save109

--- SA/Mascaras/FornecedorMascara.js
+++ SA/Mascaras/FornecedorMascara.js
@@ -3,39 +3,35 @@ function mascara(o, f) {
     let objeto = o;
     let funcao = f;
     setTimeout(function () {
-        executaMascara(objeto, funcao);
+        objeto.value = funcao(objeto.value);
     }, 1);
 }
 
-function executaMascara(objeto, funcao) {
-    objeto.value = funcao(objeto.value);
-}
-
-// Máscara de telefone: (XX) XXXXX-XXXX
+// Telefone: (XX) XXXXX-XXXX
 function Telefone(valor) {
-    valor = valor.replace(/\D/g, ""); // Remove não números
+    valor = valor.replace(/\D/g, ""); 
     valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2");
     valor = valor.replace(/(\d{5})(\d{4})$/, "$1-$2");
     return valor;
 }
 
-// Apenas letras e espaços
+// Letras e espaços
 function ApenasLetras(valor) {
     return valor.replace(/[^a-zA-ZÀ-ú\s]/g, "");
 }
 
-// Email: remove espaços e minúsculas
+// Email simples: remove espaços e lowercase
 function EmailMascara(valor) {
     valor = valor.replace(/\s/g, "");
     return valor.toLowerCase();
 }
 
-// Validação de email antes do envio
+// Valida email antes de enviar
 function validarEmail() {
     const email = document.querySelector('#Email').value;
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!regex.test(email)) {
-        alert("Por favor, insira um e-mail válido no formato exemplo@dominio.com");
+        alert("Por favor, insira um e-mail válido (ex: exemplo@gmail.com)");
         return false;
     }
     return true;
